795422ec640dae5f6d3f1f3f457fc5daf480d46a
victorscdev <victorsc.job@gmail.com>
2023-11-03 09:18:43 -0300

    add logic desk and web

--- pwa.js
+++ pwa.js
@@ -3,17 +3,16 @@ if ('serviceWorker' in navigator) {
         .then((reg) => console.log('Service Worker registrado com sucesso', reg))
         .catch((err) => console.log('Erro ao registrar o Service Worker', err));
 }
-console.log(window.location.includes(orion-tech-desk.vercel));
 
-// const nav_links = document.querySelectorAll(".navbar .nav-link")
+const nav_links = document.querySelectorAll(".navbar .nav-link")
 
-// if (!window.matchMedia('(display-mode: standalone)').matches) {
-//     nav_links.forEach((link) => {
-//         if(link.id === "funcionarios") link.remove()
-//     })
-// } else {
-//     nav_links.forEach((link) => {
-//         if(link.id === "apontamento_horas") link.remove()
-//         if(link.id === "demonstrativo_ferias") link.remove()
-//     })
-//   }
+if (window.location.href.includes("orion-tech-desk")) {
+    nav_links.forEach((link) => {
+        if (link.id === "funcionarios") link.remove()
+    })
+} else if(window.location.href.includes("orion-tech-web")) {
+    nav_links.forEach((link) => {
+        if (link.id === "apontamento_horas") link.remove()
+        if (link.id === "demonstrativo_ferias") link.remove()
+    })
+}
