49a353bd1297fd1874991ae42a6cdc17a57f4d25
victorscdev <victorsc.job@gmail.com>
2023-11-03 09:28:59 -0300

    ajuste

--- pwa.js
+++ pwa.js
@@ -1,17 +1,17 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then((reg) => console.log('Service Worker registrado com sucesso', reg))
-        .catch((err) => console.log('Erro ao registrar o Service Worker', err));
-}
-
 const nav_links = document.querySelectorAll(".navbar .nav-link")
 
 if (window.location.href.includes("orion-tech-desk")) {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js')
+            .then((reg) => console.log('Service Worker registrado com sucesso', reg))
+            .catch((err) => console.log('Erro ao registrar o Service Worker', err));
+    }
+    
     nav_links.forEach((link) => {
         if (link.id === "apontamento_horas") link.remove()
         if (link.id === "demonstrativo_ferias") link.remove()
     })
-} else if(window.location.href.includes("orion-tech-web")) {
+} else {
     nav_links.forEach((link) => {
         if (link.id === "funcionarios") link.remove()
     })
--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('my-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                './assets/styles/import.css',
-                '/pwa.js'
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    // event.respondWith(
-    //     caches.match(event.request).then((response) => {
-    //         return response || fetch(event.request);
-    //     })
-    // );
-});
+if (window.location.href.includes("orion-tech-desk")) {
+    self.addEventListener('install', (event) => {
+        event.waitUntil(
+            caches.open('my-cache').then((cache) => {
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                    './assets/styles/import.css',
+                    '/pwa.js'
+                ]);
+            })
+        );
+    });
+    
+    self.addEventListener('fetch', (event) => {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    });
+}
